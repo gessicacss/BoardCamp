6ef65b5e57d378cb8b4b2abeddd13f6740024bd7
Gessica <santos.gessicacarolina@gmail.com>
2023-05-09 22:37:53 -0300

    feat: add input validation and error handling for game creation

--- src/routes/games.routes.js
+++ src/routes/games.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { createGames, getGames } from "../controllers/games.controllers.js";
+import gameSchema from "../schemas/games.schemas.js";
+import validateSchema from "../middlewares/validateSchema.middleware.js";
 
 const gamesRouter = Router()
 
 gamesRouter.get("/games", getGames);
-gamesRouter.post("/games", createGames);
+gamesRouter.post("/games", validateSchema(gameSchema), createGames);
 
 export default gamesRouter;
---
+++ src/schemas/games.schemas.js
@@ -0,0 +1,10 @@
+import joi from 'joi';
+
+const gameSchema = joi.object({
+    name: joi.string().required(),
+    image: joi.string().required(),
+    stockTotal: joi.number().required().min(1),
+    pricePerDay: joi.number().required().min(1)
+})
+
+export default gameSchema;
