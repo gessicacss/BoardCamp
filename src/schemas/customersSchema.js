4b235a8aefc9bfe3361d6bc1d5314f029826e721
Gessica <santos.gessicacarolina@gmail.com>
2023-05-11 22:41:43 -0300

    fix: fix birthday on customers schema

--- src/schemas/customersSchema.js
+++ src/schemas/customersSchema.js
@@ -4,7 +4,7 @@ const customerSchema = joi.object({
     name: joi.string().trim().required(),
     phone: joi.string().trim().pattern(/^\d{10,11}$/).required(),
     cpf: joi.string().trim().length(11).pattern(/^\d+$/).required(),
-    birthday: joi.string().pattern(/^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/).required()
+    birthday: joi.date().required()
 })
 
 export default customerSchema;
